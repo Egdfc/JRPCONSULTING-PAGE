b83475f85dc3794b34b6a498cddd0f2208e85aae
Emanuel Flores <efcbasketcase@gmail.com>
2023-05-04 10:41:55 -0600

    "quite padding navbar"

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll",function(){
 var header = this.document.querySelector(".navegacionbar");
-header.classList.toggle("sticky",window.scrollY> 0);
+header.classList.toggle("sticky",window.scrollY > 0);
 })
