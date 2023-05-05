ea3438f9d904c02d2e647bb63304e43935975bc4
Emanuel Flores <efcbasketcase@gmail.com>
2023-05-05 14:07:43 -0600

    boton enviar contacto dinamico

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,3 +4,28 @@ header.classList.toggle("sticky",window.scrollY> 0);
 })
 
 
+function Comprobar(obj){
+  if ( !obj.acuerdo[0].checked )
+  {
+       alert("Debes estar de acuerdo")
+        return false;
+  }
+  return true 
+}
+
+
+var name= document.getElementById('name');
+var email= document.getElementById('email');
+var phone = document.getElementById('phone');
+var message=document.getElementById('message');
+
+
+var btnEnviar=document.getElementById('submitButton');
+
+btnEnviar.onclick = presionaEnviar;
+
+function presionaEnviar(){
+    let formulario=document.getElementById('contactForm');
+    return formulario;
+
+}
