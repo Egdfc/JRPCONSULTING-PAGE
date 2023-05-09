88ff1256ef29ba094a288e4b4db9bb1ee5f9b7fe
Emanuel Flores <efcbasketcase@gmail.com>
2023-05-09 15:57:46 -0600

    opacidad

--- dist/php/enviar.php
+++ dist/php/enviar.php
@@ -9,9 +9,13 @@
                         "Email: ".$email"<br>" 
                         "Mensaje:".$mensaje;
 
-
-	mail('ventas@jprconsulting.com', $asunto, $mensaje)
+	if(mail('ventas@jprconsulting.com', $asunto, $mensaje)){
 		echo "<script>alert("correo enviado exitosamente")</script>" ;
-        echo "<script>setTimeout(\"location.href='contact.html'\",500)</script>" ;
+		echo "<script>setTimeout(\"location.href='contact.html'\",500)</script>" ;
+	}else{
+		echo "<script>alert("No se pudo enviar intente mas tarde")</script>" ;
+		echo "<script>setTimeout(\"location.href='contact.html'\",500)</script>" ;
+	}
+	
 
  ?>
